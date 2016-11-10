508f6b0d04c1b99cb9c182b474b6a92ce55a4852
Jen Doyle <jendoyledesign@gmail.com>
2016-11-10 18:15:56 -0500

    CSS Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+
   var $toggleButton = $('.toggle-button');
   $toggleButton.on('click', function() {
     $(this).toggleClass('button-open');
   });
+
+   $("#load-more").on("click", function (){
+        $("#show-this-on-click").slideDown();
+        $("#load-more").hide();
+    });
+
+
+
+
   
 });

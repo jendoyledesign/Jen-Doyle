b1b2323fcca39884a9c545f4048e47b06c00e9b5
Jen Doyle <jendoyledesign@gmail.com>
2017-11-01 14:42:51 -0400

    Updated Cname and 185 other files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	var $toggleButton = $('.toggle-button'),
+    	$menuWrap = $('.menu-wrap'),
+    	$sidebarArrow = $('.sidebar-menu-arrow');
+
+	// Hamburger button
+
+	$toggleButton.on('click', function() {
+		$(this).toggleClass('button-open');
+		$menuWrap.toggleClass('menu-show');
+	});
+
+
+});

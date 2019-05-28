64f90682dd587b3457eb64469ff49efee6840894
Jen Doyle <jendoyledesign@gmail.com>
2019-05-28 15:26:30 -0400

    about

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
